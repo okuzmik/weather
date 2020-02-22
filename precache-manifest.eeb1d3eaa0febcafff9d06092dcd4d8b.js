d1ba9d083bed46c4480803bc105c21d23b06c340
okuzmik <okuzmik@tlen.pl>
2020-02-22 10:50:53 +0100

    Updates

---
+++ precache-manifest.eeb1d3eaa0febcafff9d06092dcd4d8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d72475526c830393f2b502c7bdaa8cd1",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "48dd07e8a10fee6fe0d5",
+    "url": "/Weather-App/static/css/main.a3724cf2.chunk.css"
+  },
+  {
+    "revision": "80554dce1d14e3174b47",
+    "url": "/Weather-App/static/js/2.2e89cbf8.chunk.js"
+  },
+  {
+    "revision": "48dd07e8a10fee6fe0d5",
+    "url": "/Weather-App/static/js/main.17b2e745.chunk.js"
+  },
+  {
+    "revision": "cd226f0229fb97daef04",
+    "url": "/Weather-App/static/js/runtime~main.db1e35d1.js"
+  },
+  {
+    "revision": "e054f892490e44d2745c25f3d487ce3f",
+    "url": "/Weather-App/static/media/dark.e054f892.png"
+  },
+  {
+    "revision": "23e2b6cf75076c68a788053631d1f14d",
+    "url": "/Weather-App/static/media/dark_media.23e2b6cf.png"
+  }
+]);
